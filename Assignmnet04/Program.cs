14532bc81cfbf622f7da961ed4f6d231d274b0a6
VincentDinh2305 <90953477+VincentDinh2305@users.noreply.github.com>
2022-02-16 06:01:34 -0500

    Add files via upload

---
+++ Assignmnet04/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Writable writable = new Writable();
+            using (TextWriter writer = File.CreateText("d:\\test.txt"))
+            {
+                writable.write(writer);
+            }
+            Console.WriteLine("File Created Success.");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Assignmnet04/Writable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment04
+{
+    public interface IWritable
+    {
+        void write(TextWriter writer);
+    }
+    public class Writable : IWritable
+    {
+        public void write(TextWriter writer)
+        {
+            writer.WriteLine("Hello Wold");
+            writer.Write("Printing Using Write........");
+        }
+    }
+}
